685716cd9286923821a5b9ce8f4dc10860a1212c
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 00:04:20 +0800

    查看

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<div><h4>Huya, Douyu, Douyin, YY Live Agent</h4></div><p>Access path: <a href:"https://baidu.com">https://jkio.ml/<platform>/id</a></p><p><h4>GitHub repository</h4></p>")
+  fmt.Fprintf(w, "<div><h4>Huya, Douyu, Douyin, YY Live Agent</h4></div> <p>Access path: https://jkio.ml/<platform>/id</p> <p><h4>GitHub repository</h4></p>")
   return
 }
