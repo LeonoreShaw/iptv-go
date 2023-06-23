47df48c5ab7e00e88df1c663e3817afb293f752d
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 00:15:16 +0800

    居中怎么改

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Huya, Douyu, Douyin, YY Live Agent</h1> <p>Access path: <i>https://jkio.ml/platform/id</i></p> <p><h4>Learn more about it from GitHub Repository: <i>https://github.com/LeonoreShaw/iptv-go</i></h4></p>")
+  fmt.Fprintf(w, "<h1>Huya, Douyu, Douyin, YY Live Agent</h1> <p>Access path: <i>https://jkio.ml/platform/id</i></p> <p><h6>Learn more about it from GitHub Repository: <i>https://github.com/LeonoreShaw/iptv-go</i></h6></p>")
   return
 }
