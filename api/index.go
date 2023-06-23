0606a2b3e06cd12f73639f5d9580ac5f79e7d3a1
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-23 23:26:22 +0800

    更改index主页。

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1 text-align:center>虎牙、斗鱼、、抖音、YY直播代理</h1><center>访问路径：https://jkio.ml/live/<平台>/id</center><h2 text-align:center>GitHub仓库地址：https://github.com/leonoreshaw/iptv-go/</center>")
+  fmt.Fprintf(w, "<h1 align="center">Huya, Douyu, , Douyin, YY Live Agent</h1><center>Access path: https://jkio.ml/live/<platform>/id</center><h2 align="center">GitHub repository: https://github.com/leonoreshaw/iptv-go/</center>")
   return
 }
