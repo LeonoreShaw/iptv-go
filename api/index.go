1c845aeac05a887a15f871e6e9a6a7c44a9b43f4
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 00:13:12 +0800

    居中一个了

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Huya, Douyu, Douyin, YY Live Agent</h1> <p>Access path: <i>https://jkio.ml/platform/id</i></p> <p><h4>Learn more about it from GitHub Repository: <i>https://github.com/LeonoreShaw/iptv-go</i></h4></p>")
+  fmt.Fprintf(w, "<h1 align="center">Huya, Douyu, Douyin, YY Live Agent</h1> <p>Access path: <i>https://jkio.ml/platform/id</i></p> <p><h4>Learn more about it from GitHub Repository: <i>https://github.com/LeonoreShaw/iptv-go</i></h4></p>")
   return
 }
