35497fd175806778126ce9f27356b6e018cf03b2
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-23 23:59:50 +0800

    页面

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h2>Huya, Douyu, Douyin, YY Live Agent</h2><h4>Access path</h4><h4>GitHub repository</h4>")
+  fmt.Fprintf(w, "<div><h1>Huya, Douyu, Douyin, YY Live Agent</h1></div><p><h4>Access path</h4></p><p><h4>GitHub repository</h4></p>")
   return
 }
