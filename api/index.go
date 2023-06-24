fd741863190b437c2c1fb684bce15248bdb5e73d
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 12:44:27 +0800

    完成自定义。

--- api/index.go
+++ api/index.go
@@ -16,7 +16,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
   "<h1><center>Huya, Douyu, Douyin, YY Live Agent</center></h1>"+
   "<p style=\"font-weight: bold;\"><center>Access path: <i>https://jkio.ml/platform/id</i></center></p>"+
   "<h6><center>Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\" target=\"_blank\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
-  "<hr style=\"border: 1px; border-top: 2px solid blue;\">"+
+
   "<h1><center>虎牙、斗鱼、抖音、YY直播代理</center></h1>"+
   "<p style=\"font-weight: bold;\"><center>访问路径：<i>https://jkio.ml/platform/id</i></center></p>"+
   "<h6><center>在 GitHub 仓库了解更多信息：<i><a href=\"https://github.com/LeonoreShaw/iptv-go\" target=\"_blank\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
