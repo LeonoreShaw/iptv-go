6bcedc2c56312e3154c258509b029af23b3249a6
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 13:17:55 +0800

    完成。

--- api/index.go
+++ api/index.go
@@ -21,9 +21,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
   "<h1><center>虎牙、斗鱼、抖音、YY直播代理</center></h1>"+
   "<p style=\"font-weight: bold;\"><center>访问路径：<i>https://jkio.ml/platform/id</i></center></p>"+
   "<h6><center>在 GitHub 仓库了解更多信息：<i><a href=\"https://github.com/LeonoreShaw/iptv-go\" target=\"_blank\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
-  "<hr style=\"border: 5px; border-top: 10px\">"+
+  "<hr style=\"border: 10px; border-top: 10px\">"+
   "<hr style=\"border: 5px; border-top: 2px solid pink;\">"+
-  "<hr style=\"border: 5px; border-top: 10px\">"+
+  "<hr style=\"border: 10px; border-top: 10px\">"+
+  "<p><center>Live Stream on <a href=\"https://xxiao.org\" target=\"_blank\" style=\"color: hotpink\">xxiao.org</a>.</center></p>"+
   "<p><center>&copy; 2020-2023 <a href=\"https://xxiao.org\" target=\"_blank\" style=\"color: hotpink\">Léonore</a> All rights reserved.</center></p>"+
   "</body>"+
   "</html>")
