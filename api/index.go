d5bacac0c68e2e9eecdc6d3082e5caed85519721
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 11:39:27 +0800

    fenghang...

--- api/index.go
+++ api/index.go
@@ -6,6 +6,10 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<body style=\"display: flex; align-items: center; flex-direction: column; height: 100vh;\"><h1 style=\"text-align: center;\">Huya, Douyu, &#x4F60;&#x597D;&#xFF0C;&#x4E16;&#x754C;&#xFF01; Douyin, YY Live Agent</h1> <p style=\"text-align: center;\"><i>Access path: https://jkio.ml/platform/id</i></p> <h6 style=\"text-align: center;\"><i>Learn more about it from GitHub Repository: <a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></h6></body>")
+  fmt.Fprintf(w, "<body style=\"display: flex; align-items: center; flex-direction: column; height: 100vh;\">
+  <h1 style=\"text-align: center;\">Huya, Douyu, &#x4F60;&#x597D;&#xFF0C;&#x4E16;&#x754C;&#xFF01; Douyin, YY Live Agent</h1>
+  <p style=\"text-align: center;\"><i>Access path: https://jkio.ml/platform/id</i></p> 
+  <h6 style=\"text-align: center;\">Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></h6>
+  </body>")
   return
 }
