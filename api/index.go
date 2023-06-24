9d294a5e230840387e7fe55e6dad36cd89e64c83
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 11:49:52 +0800

    huanhang

--- api/index.go
+++ api/index.go
@@ -6,7 +6,7 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<body style=\"display: flex; align-items: center; justify-content: center; height: 100vh;\">"+
+  fmt.Fprintf(w, "<body style=\"display: flex; align-items: center; justify-content: center; \">"+
   "<h1 style=\"text-align: center;\">Huya, Douyu, &#x4F60;&#x597D;&#xFF0C;&#x4E16;&#x754C;&#xFF01; Douyin, YY Live Agent</h1>\n"+
   "<p style=\"text-align: center;\">Access path: <i>https://jkio.ml/platform/id</i></p>\n "+
   "<h6 style=\"text-align: center;\">Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></h6>\n"+
