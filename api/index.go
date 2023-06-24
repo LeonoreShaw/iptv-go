983c05462e4ac0eddb5c9920338dc2897035f2e2
Léonore Shaw <leonoreshaw@gmail.com>
2023-06-24 12:33:36 +0800

    完成自定义index页面。

--- api/index.go
+++ api/index.go
@@ -10,16 +10,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
   "<html>"+
   "<head>" +
   "<meta charset=\"UTF-8\">" +
-  "<title>My Page</title>" +
+  "<title>Live Stream on xxiao.org</title>" +
   "</head>" +
   "<body style=\"display: flex; align-items: center; flex-direction: column; font-size: 24px; font-family: 'Microsoft YaHei', sans-serif;\">"+
   "<h1><center>Huya, Douyu, Douyin, YY Live Agent</center></h1>"+
   "<p style=\"font-weight: bold;\"><center>Access path: <i>https://jkio.ml/platform/id</i></center></p>"+
   "<h6><center>Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
-  "<hr>"+
-  "<h1><center>Huya, Douyu, &#x4F60;&#x597D;&#xFF0C;&#x4E16;&#x754C;&#xFF01; Douyin, YY Live Agent</center></h1>"+
-  "<p style=\"font-weight: bold;\"><center>Access path: <i>https://jkio.ml/platform/id</i></center></p>"+
-  "<h6><center>中文Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
+  "<hr style=\"border: 1px; border-top: 2px solid blue;\">"+
+  "<h1><center>虎牙、斗鱼、抖音、YY直播代理</center></h1>"+
+  "<p style=\"font-weight: bold;\"><center>访问路径：<i>https://jkio.ml/platform/id</i></center></p>"+
+  "<h6><center>在 GitHub 仓库了解更多信息：<i><a href=\"https://github.com/LeonoreShaw/iptv-go\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
   "</body>"+
   "</html>")
   return
