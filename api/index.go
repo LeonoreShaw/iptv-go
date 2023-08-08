0a77294809aedc113824aacacf6e125b2a09d243
Léonore Shaw <leonoreshaw@gmail.com>
2023-08-08 21:45:50 +0800

    修改域名为l.shaw.cf

--- api/index.go
+++ api/index.go
@@ -15,11 +15,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
   "</head>" +
   "<body style=\"font-size: 24px; \">"+
   "<h1><center>Huya, Douyu, Douyin, YY Live Agent</center></h1>"+
-  "<p style=\"font-weight: bold;\"><center>Access path: <i>https://l.jkio.cf/platform/id</i></center></p>"+
+  "<p style=\"font-weight: bold;\"><center>Access path: <i>https://l.shaw.cf/platform/id</i></center></p>"+
   "<h6><center>Learn more about it from GitHub Repository: <i><a href=\"https://github.com/LeonoreShaw/iptv-go\" target=\"_blank\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
   "<hr style=\"border: 5px; border-top: 10px\">"+
   "<h1><center>虎牙、斗鱼、抖音、YY直播代理</center></h1>"+
-  "<p style=\"font-weight: bold;\"><center>访问路径：<i>https://l.jkio.cf/platform/id</i></center></p>"+
+  "<p style=\"font-weight: bold;\"><center>访问路径：<i>https://l.shaw.cf/platform/id</i></center></p>"+
   "<h6><center>在 GitHub 仓库了解更多信息：<i><a href=\"https://github.com/LeonoreShaw/iptv-go\" target=\"_blank\">https://github.com/LeonoreShaw/iptv-go</a></i></center></h6>"+
   "<hr style=\"border: 10px; border-top: 10px\">"+
   "<hr style=\"border: 5px; border-top: 2px \">"+
